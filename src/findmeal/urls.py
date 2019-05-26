cfa493cee0b18eab0fd9ca3ced75e208ba95fc68
ha3 <h.ozdemir13@yandex.com>
2019-05-26 19:07:12 +0300

    Add list recipe page

--- src/findmeal/urls.py
+++ src/findmeal/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('recipe/<int:pk>/<str:slug>/', views.DetailView.as_view(), name='detail'),
     path('<int:recipe_id>/rate/', views.rate, name='rate'),
-    path('listing/', views.listIngredients, name='listing')
+    path('listing/', views.ListIngredients, name='listing'),
+    path('search/', views.SearchView.as_view(), name='search')
 ]
