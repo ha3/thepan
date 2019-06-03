796f2a13e50a85982e3323896806dad02526b23d
ha3 <h.ozdemir13@yandex.com>
2019-06-03 15:32:49 +0300

    Upgrade custom template tag by removing unnecessary codes.

--- src/findmeal/templatetags/utilities.py
+++ src/findmeal/templatetags/utilities.py
@@ -1,3 +1,8 @@
+"""
+This is a heavily edited version of a custom template tag which I found at
+https://www.silviogutierrez.com/blog/smarter-django-url-tag/
+"""
+
 from django import template
 from django.template.defaulttags import URLNode, url as url_tag
 
@@ -9,44 +14,37 @@ def url(parser, token):
     return SmartURLNode(validator.view_name, validator.args, validator.kwargs, validator.asvar)
 
 class SmartURL(str):
+    # This is a wrapper class that allows us to attach attributes to strings.
     pass
 
 class SmartURLNode(URLNode):
     def render(self, context):
-        resolved_view_name = self.view_name.resolve(context)
+        view_name = self.view_name.resolve(context)
+        view_name_string = "'%s'" % view_name
 
-        if resolved_view_name:
-            view_name_string = "'%s'" % resolved_view_name
-        else:
-            view_name_string = unicode(self.view_name)
-
+        #If it is a hard-coded url, strip it from the commas and assign to context variable
         if len(view_name_string) >= 3 and view_name_string[0] == view_name_string[-1] and view_name_string[0] in ('"', "'") and view_name_string[1] == '/':
-            rendered = view_name_string[1:-1]
+            context[self.asvar] = view_name_string[1:-1]
 
-            if self.asvar:
-                context[self.asvar] = rendered
-
+        # If not, use the standard url logic.
         else:
-            rendered = super(SmartURLNode, self).render(context)
+            super(SmartURLNode, self).render(context)
 
-        if not self.asvar:
-            return rendered
+        resolved_url = SmartURL(context[self.asvar])
+        request = context.get('request', None)
 
-        else:
-            resolved_url = SmartURL(context[self.asvar])
-            request = context.get('request', None)
+        if request:
+            # If the request path matches the url exactly, 'active' attr is set as 'active' on the resolved_url instance.
+            if resolved_url == request.path:
+                resolved_url.active = 'active'
+            else:
+                resolved_url.active = ''
 
-            if request:
-                # Step 7
-                if resolved_url == request.path:
-                    resolved_url.active = 'active'
-                else:
-                    resolved_url.active = ''
+            # If the request path starts with the url, 'active_root' attr is set as 'active' on the resolved_url instance.
+            if request.path.startswith(resolved_url):
+                resolved_url.active_root = 'active'
+            else:
+                resolved_url.active_root = ''
 
-                if request.path.startswith(resolved_url):
-                    resolved_url.active_root = 'active'
-                else:
-                    resolved_url.active_root = ''
-
-            context[self.asvar] = resolved_url
-            return ''
+        context[self.asvar] = resolved_url
+        return ''
