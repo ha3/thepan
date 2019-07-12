6e3521c930e6ea4af56cbe996c2e13c955fd8d97
ha3 <h.ozdemir13@yandex.com>
2019-07-12 17:47:59 +0300

    Rewrite SelectTimeWidget

--- src/findmeal/admin.py
+++ src/findmeal/admin.py
@@ -7,7 +7,6 @@ from .widgets import SelectTimeWidget
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
 
-
 class RecipeStepInline(admin.StackedInline):
     model = RecipeStep
 
@@ -15,9 +14,8 @@ class RecipeAdmin(admin.ModelAdmin):
     inlines = [RecipeStepInline, RecipeIngredientInline]
     readonly_fields = ('rating', 'rate_count',)
     formfield_overrides = {
-        models.DurationField: {'widget': SelectTimeWidget(minute_step=5, use_seconds=False)},
+        models.DurationField: {'widget': SelectTimeWidget(minute_step=5)},
     }
-
 
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ingredient)
