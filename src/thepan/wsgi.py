c13248bf13865000e00d53255dbac3e3f497e106
ha3 <h.ozdemir13@yandex.com>
2019-08-02 17:46:30 +0300

    Load related settings file with dotenv

--- src/thepan/wsgi.py
+++ src/thepan/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for thepan project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
-"""
-
 import os
+from dotenv import load_dotenv
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thepan.settings')
+load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thepan.settings.development')
 
 application = get_wsgi_application()
